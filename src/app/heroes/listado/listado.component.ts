f695ed2bddc4cd7f0ebfa2a40d619c73196ad865
Rocío Colado <r.colado.chaves@gmail.com>
2022-11-07 13:02:03 +0100

    first commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+  heroes: string[]= ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Batman'];
+  heroeBorrado: string = '';
+
+  borrarHeroe() {
+    this.heroeBorrado = this.heroes.pop() || '';
+  }
+
+}
