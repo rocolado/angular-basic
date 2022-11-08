18160c6b544a72ee8c8a204dc4418b839a5c2f07
Rocío Colado <r.colado.chaves@gmail.com>
2022-11-08 11:19:04 +0100

    third commit

--- src/app/dbz/agregar/agregar.component.ts
+++ src/app/dbz/agregar/agregar.component.ts
@@ -2,6 +2,7 @@ import { outputAst } from '@angular/compiler';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
     selector: 'app-agregar',
@@ -13,15 +14,17 @@ import { Personaje } from '../interfaces/dbz.interface';
         poder: 0
     }
 
-    @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+    constructor(private dbzService: DbzService) {}
+
+    // @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
     
     agregar() {
         if (this.nuevo.nombre.trim().length === 0) {
           return;
         }
 
-        console.log(this.nuevo);
-        this.onNuevoPersonaje.emit(this.nuevo);
+        // this.onNuevoPersonaje.emit(this.nuevo);
+        this.dbzService.agregarPersonaje(this.nuevo);
     
         this.nuevo = {
           nombre: '',
